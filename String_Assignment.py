26870ebf8232faece9a302585c23b7c4a8dfc0c1
dnyaneshwari_varal <varaldnyaneshwari@gmail.com>
2025-08-29 12:15:10 +0530

    python

--- String_Assignment.py
+++ String_Assignment.py
@@ -1,15 +1,16 @@
 #que_1
 
 import re
-print('''1.split the string into words while removing punchuation
-2.join the words back into a sentence with a custom separation(' | ')
-3.Partition the string based on the first occurrence of that character 
-4.reverce the words order using split() and join
-5.count vowels and consonants,numbers,special characters
-6.remove Duplicate characters
-7.count a specific character in string''')
-#text=input("Enter text: ")
-for i in range(7):
+  
+while True:
+    print('''1.split the string into words while removing punchuation
+         2.join the words back into a sentence with a custom separation(' | ')
+         3.Partition the string based on the first occurrence of that character 
+         4.reverce the words order using split() and join
+         5.count vowels and consonants,numbers,special characters
+         6.remove Duplicate characters
+         7.count a specific character in string''')
+
     text=input("Enter text: ")
     choice=int(input("ENTER YOUR CHOICE: "))
     
@@ -17,42 +18,30 @@ for i in range(7):
     if(choice == 1):
         print("split the string into words while removing punchuation: ")
         #print(str.split())
-        #re.sub(pattern(r'[^\w\s]' replace anything not repace a-z A-Z 0-9,underscore), replacement('' means replace with nothing), text)
-        words=re.sub(r'[^\w\s]','',text).split()
-        print(words)
-        #re.sub means replacement substitute
+        #print(words)
+        for ch in text:
+           if ch.isalnum() or ch ==' ':
+               print(ch, end=" ")
+        print() 
+        
     
     if(choice==2):
+        print("2.join the words back into a sentence with a custom separation(' | ')")
+        
         print('|'.join(words))
 
-    if(choice==3):
-        first_char=text[0]
-        words=text.split()
-
-        part=[]
-        current=""
-        for i in words:
-            if i[0]==first_char and current !="":
-                part.append(current.strip())
-                current=i
-            else:
-                if current:
-                    current += " "+i
-                else:
-                    current =i
-        part.append(current.strip())
-
-        for j in part:
-            print(part)
-
-
-    if(choice==4):
+    elif(choice==3):
+        print("3.Partition the string based on the first word occurrence of that character ")
+        words=input("enter seperator: ")
+        part=text.partition(words)
+        print("partition of string is: ",part)
+    elif(choice==4):
         word=text.split()
         word.reverse()
-        reversed_text=' '.join(word)
+        reversed_text='|'.join(word)
         print(reversed_text)
 
-    if(choice==5):
+    elif(choice==5):
         text=text.lower()
         vowels=0
         consonants=0
@@ -75,7 +64,7 @@ for i in range(7):
         print("count of special char is: ",special)
 
 
-    if(choice == 6):
+    elif(choice == 6):
         result=""
         for ch in text:
             if ch  not in result:
@@ -83,7 +72,7 @@ for i in range(7):
         print("original text: ",text)
         print("after removing duplicate: ",result)
 
-    if(choice==7):
+    elif(choice==7):
         specific_char=input("Enter specif character: ")
         count=0
         for ch in text:
