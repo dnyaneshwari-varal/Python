cd8d3e1a78a696fae69d6780ebf0242075299432
dnyaneshwari_varal <varaldnyaneshwari@gmail.com>
2025-09-02 21:34:23 +0530

    Dictionary

--- Dict_Assignment.py
+++ Dict_Assignment.py
@@ -1,27 +1,6 @@
 '''
-def student_dict(d1):
- for i in d1:
-   #print(i,d1[i])
-   if(d1[i]>=90 and d1[i]<=100):
-       d1[i]="A"
-   elif(d1[i]>=80 and d1[i]<=89):
-       d1[i]="B"
-   elif(d1[i]>=70 and d1[i]<=79):
-       d1[i]="C"
-   elif(d1[i]>=60 and d1[i]<=69):
-       d1[i]="D"
-   else:
-       d1[i]="F"
- return d1
-    
-       
+#code1
 
-
-d1={"Ram":89,"Sham":57,"Prem":67,"Raj":87,"Siya":90}
-result=student_dict(d1)
-print(result,end='\n')
-'''
-'''
 keys=int(input("Enter total keys in dictionary: "))
 d1={}
 #dict_grade={}
@@ -50,6 +29,7 @@ for i in d1:
        d1[i]="F"
        student_grade[i]=d1[i]
 print(student_grade)
+#output
 
 Enter total keys in dictionary: 3
 Enter Key: ram
@@ -61,10 +41,10 @@ Enter Value: 89
 {'ram': 58, 'radha': 57, 'raj': 89}
 {'ram': 'F', 'radha': 'F', 'raj': 'B'}
 
-'''
 
-'''
-#2
+
+
+# code 2
 text=input("Enter Text: ")
 text=text.lower()
 #keys are words and values are there occurance count
@@ -82,8 +62,8 @@ for i in words:
   dict1[i]=count
 print(dict1) 
 
-'''
-#3
+
+#code 3
 dict={
     'SI9012340':{"Name":"Amit","Cource":"FSD-DS"},
     'SI9012341':{"Name":"Raj","Cource":"FSD-DS"},
@@ -101,58 +81,168 @@ for i in dict:
 
 
 
-#4
-    '''
-total_key=int(input("Enter Total keys: "))
-for i in range(total_keys):
-    key=int(input("Enter Key: "))
-'''
 
+#4 incomplete
 
 #take input as  how many sales men work in specific region
 #region are north,south,east,west we need to choose it
 #get data of sales men and then make distinary as north is key and {all sales men data}
 
-region=input("Enter region: ")
-sales_men=int(input("Enter how many member works: "))
 
 
-s_1={}
-s_2={}
-s_3={}
-s_4={}
+regions=["North","South","East","West"]
+student_per_region=int(input("Enter how many member works: "))
+
+students={}
+
+for region in regions:
+    students[region]={}
+    for j in range(1,student_per_region+1):
+        sid=f"{region}_{j}"
+        name=input("Enter Name: ")
+        sal=int(input("Enter sal: "))
+        students[region][sid]={"SID":sid,"Name":name,"Salary":sal}
+
+for region,student_dict in students.items():
+        print(f"\nRegion:{region}")
+        for sid,detail in student_dict.items():
+            print(detail)
+print("1.Display all sales person data")
+print("2.Add new sales person data")
+print("3.Update sales person data")
+print("4.Delete sale person data")
+choice=int(input("Enter choice: "))
+if choice==1:
+    for region,student_dict in students.items():
+        print(f"\nRegion:{region}")
+        for sid,detail in student_dict.items():
+            print(detail)
 
 
 
 
-for i in range(sales_men):
-    Id=int(input("enter sales_men Id: "))
-    name=input("Enter Name: ")
-    sal=int(input("Enter sal: "))
-    s_1={"Id":Id,"Name":name,"Salary":sal}
-print(s_1)
+
+
+#5
+Employee={}
+Total_emp=int(input("Enter total Emloyees: "))
+
+for i in range(1,Total_emp+1):
+    emp_id=int(input("Enter Empoyee ID: "))
+    emp_name=input("Enter employee name: ")
+    Dep=input("Enter employee Department: ")
+    sal=int(input("Enter Salary: "))
+    Employee[i]={"ID":emp_id,"Name":emp_name,"Department":Dep,"Salary":sal}
+    print(Employee[i])
+
+for j in range(1,Total_emp):
+        print(Employee[j])
+
+
+to_search=input("Enter Department name to search: ")
+count=0
+for i in Employee:
+    #print(i)
+    #print(Employee[i]["Department"])
+    if Employee[i]["Department"]==to_search:
     
+         count+=1
+         print(count,Employee[i])
+       
+print(count)
+new_dict={}
+for i in Employee:
+    #print(Employee[i]["Salary"])
+    if Employee[i]["Salary"]>10000:
+        new_dict[i]=Employee[i]
+print(new_dict)
+     
+
+#6
+#key becomes value value become key
 
 
+dict1={1:"Dnayneshwari",2:"Isha",3:"Sham",4:"Hanu"}
+inverted_dict={}
+for key in dict1:
+     value=dict1[key]
+     inverted_dict[value]=key
+    
+    
+    
+print(inverted_dict)
+   
+
+#6
+#Merge Dictionaries
+#Take two dictionaries as input and merge them.
+#If a key is common in both, add their values instead of replacing.
+
+dict1={}
+dict2={}
+items=int(input("How many items for dict: "))
+for i in range(items):
+    
+    fruit_name=input("Enter Fruit name: ")  #key
+    fruit_District=input("Enter country: ") #value
+    
+    dict1[fruit_name]=fruit_District
+    
+print(dict1)
+
+items1=int(input("How many items for dict: "))
+for j in range(items1):
+    
+    country_name=input("Enter country: ")
+    capital_name=input("Enter Fruit name: ")
+    dict2[country_name]=capital_name
+    
+print(dict2)
+
+merge={}
+
+for i in dict1:
+    merge[i]=dict1[i]
+
+for k in dict2:
+    if k in merge:
+        merge[k] +=dict2[k]
+    else:
+        merge[k]=dict2[k]
+print("dict1 is: ",dict1)
+print("dict2 is: ",dict2)
+print("merge Dictionary: ",merge)
+
+'''
+#7 Write a program to print all unique values from a dictionary that may have duplicate values.
 
 
+d1={1:"a",2:"b",3:"c",4:"a",5:"b",6:"z"}
+result={}
+for i in d1:
+    print(d1[i])
+    if d1[i] not in result.values():
+        result[i]=d1[i]
+print(result)
 
 
+#8
+total_stud=int(input("Enter num of students: "))
+students={}
+mark_dict={}
+subject=int(input("Enter total subjects: "))
+for i in range(total_stud):
+  name=input("Enter Name: ")
+  for j in range(subject): 
+    sub=input("Enter subject: ")
+    marks=int(input("Enter marks: "))
+    mark_dict[sub]=marks
+    students[i]={name:mark_dict}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for i in students:
+    print(students[i])
+        
+        
 
 
 
